07db38119a3b77dac2c43cee76c20a3e6bc56cd2
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-09-12 14:35:08 -0400

    Added project file

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-doc-test "1.0.0-SNAPSHOT"
+  :description "clj-doc-test leiningen plugin"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [swank-clojure "1.2.1"]])
