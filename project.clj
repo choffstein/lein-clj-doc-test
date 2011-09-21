e9fb8ffbac11a13284dad2bca6b9890347052d23
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-09-21 16:08:51 -0400

    Changed versioning

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-doc-test "1.0.0-SNAPSHOT"
+(defproject clj-doc-test "0.0.1"
   :description "clj-doc-test leiningen plugin"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
