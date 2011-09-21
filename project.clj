3ba7a2aebd277dd4fb8096142bebbe697ccad274
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-09-21 16:12:48 -0400

    Project name change

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clj-doc-test "0.0.1"
-  :description "clj-doc-test leiningen plugin"
+(defproject lein-clj-doc-test "0.0.1"
+  :description "leiningen plugin for clj-doc-test"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [swank-clojure "1.2.1"]])
