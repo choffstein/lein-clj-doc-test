b3bdf5435976ae0eece16bf8b6765c76151e0104
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-11-23 00:52:59 -0500

    Update to 1.3.0

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject lein-clj-doc-test "0.0.1"
   :description "leiningen plugin for clj-doc-test"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [swank-clojure "1.2.1"]])
