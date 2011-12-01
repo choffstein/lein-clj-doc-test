c5177f5bd74b75839061172f13a6bf39c91ebd89
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-11-30 20:51:50 -0800

    Updated project file

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject lein-clj-doc-test "0.0.1"
+(defproject lein-clj-doc-test "0.0.1-SNAPSHOT"
   :description "leiningen plugin for clj-doc-test"
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [org.clojure/tools.namespace "0.1.0"]
-                 [swank-clojure "1.2.1"]])
+                 [swank-clojure "1.4.0-SNAPSHOT"
+                  :exclusions [org.clojure/clojure
+                               org.clojure/clojure-contrib]]])
