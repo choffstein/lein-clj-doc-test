fe938cdc4bef038cfb11b1bd3ce72b54d5d6465d
Corey M. Hoffstein <corey@newfoundresearch.com>
2012-03-01 16:00:53 -0500

    Added repos to project file

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject lein-clj-doc-test "0.0.1-SNAPSHOT"
   :description "leiningen plugin for clj-doc-test"
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                 [org.clojure/tools.namespace "0.1.0"]
-                 [swank-clojure "1.4.0-SNAPSHOT"
-                  :exclusions [org.clojure/clojure
-                               org.clojure/clojure-contrib]]])
+                 [org.clojure/tools.namespace "0.1.0"]]
+
+  :plugins [[s3-wagon-private "1.1.1"]]
+
+  :repositories {"nfr-releases" "s3p://newfound-mvn-repo/releases/"
+                 "nfr-snapshots" "s3p://newfound-mvn-repo/snapshots/"})
